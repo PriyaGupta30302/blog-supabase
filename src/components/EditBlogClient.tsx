6d642a7365c04677c94a4d6f45fc0201ff3cecac
Priya Gupta <priyagupta30302@gmail.com>
2026-03-03 12:30:33 +0530

    feat: create EditBlogClient component for editing blog posts.

--- src/components/EditBlogClient.tsx
+++ src/components/EditBlogClient.tsx
@@ -14,7 +14,7 @@ export default function EditBlogClient({ blog }: { blog: any }) {
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <Header />
+      <Header /> 
       <main className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="mb-8">
            <button 
